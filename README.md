3546c6d729ecaa4627eaf7d0ce98c7a84bf77e1b
Todd Wolfson <todd@twolfson.com>
2012-10-07 21:15:16 -0700

    Generating minified files

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ grunt.loadNpmTasks('grunt-jsmin-sourcemap');
 _(Coming soon)_
 
 ## Contributing
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint via [grunt][grunt] and test via `npm test`.
 
 ## License
 Copyright (c) 2012 Todd Wolfson
--- tasks/jsmin-sourcemap.js
+++ tasks/jsmin-sourcemap.js
@@ -1,5 +1,33 @@
+var jsmin = require('jsmin-sourcemap');
 module.exports = function (grunt) {
-  grunt.registerTask('jsmin-sourcemap', 'Generate minified JavaScript and sourcemap from files', function() {
-    grunt.log.write(grunt.helper('jsmin-sourcemap'));
+  grunt.registerMultiTask('jsmin-sourcemap', 'Generate minified JavaScript and sourcemap from files', function () {
+    // Grab the files to minify
+    var file = this.data,
+        srcFile = file.src,
+        srcFiles = grunt.file.expand(srcFile);
+
+    // Map each file into a JSMin input
+    var input = srcFiles.map(function (file) {
+      var code = grunt.file.read(file),
+          src = file;
+      return {'code': code, 'src': src};
+    });
+
+    // Minify the input
+    var destFile = file.dest,
+        retObj = jsmin({
+          'input': input,
+          'dest': destFile
+        });
+
+    // Grab the minified code
+    var code = retObj.code;
+
+    // Append a sourceMappingURL to the code
+    code = code + '\n//@ sourceMappingURL=' + destFile;
+
+    // Write out the code and map
+    grunt.file.write(destFile, code);
+    grunt.file.write(file.destMap, retObj.sourcemap);
   });
 };
