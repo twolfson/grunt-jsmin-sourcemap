06cbded1dcdf45a38301f102378923b3e2cdbd5e
Todd Wolfson <todd@twolfson.com>
2012-10-28 03:10:21 -0700

    Working on making nice tests

--- test/grunt.js
+++ test/grunt.js
@@ -3,6 +3,9 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     'jsmin-sourcemap': {
+      // // Compact format -- https://github.com/gruntjs/grunt/blob/master/docs/api.md#thisfile--grunttaskcurrentfile
+      // 'actual/compact.js': 'test_files/jquery.js',
+      // Normal format
       single: {
         src: 'test_files/jquery.js',
         dest: 'actual/jquery.min.js',
@@ -14,11 +17,17 @@ module.exports = function (grunt) {
         destMap: 'actual/multi.js.map'
       }
     },
-    test: {
+    vows: {
       all: '*.test.js'
     }
   });
 
+  // Load in jsmin-sourcemap
   grunt.loadTasks('../tasks');
-  grunt.registerTask('default', 'jsmin-sourcemap test');
+
+  // Load in vows test
+  grunt.loadNpmTasks('grunt-vows');
+
+  // Set up the default task
+  grunt.registerTask('default', 'jsmin-sourcemap vows');
 };
--- test/jsmin-sourcemap.test.js
+++ test/jsmin-sourcemap.test.js
@@ -1,4 +1,39 @@
-var grunt = require('grunt');
+var grunt = require('grunt'),
+    assert = require('assert');
+
+var commands = {
+  'A single file': function () {
+    // Generate paths for expected/actual files
+    var retObj = {
+          'expected': 'exepceted/jquery.min.js',
+          'actual': 'actual/jquery.min.js',
+          'actualMap': 'actual/jquery.js.map'
+        };
+    return retObj;
+  },
+  'processed via JSMin': function (topic) {
+    return topic;
+  },
+  'outputs properly minified code': function (topic) {
+    var expectedFile = grunt.file.read(topic.expected),
+        actualFile = grunt.file.read(topic.actual);
+    assert.strictEqual(expectedFile, actualFile, ' properly minifies files');
+  },
+  'properly points to the map file': function (topic) {
+    // This is handled by the outputs properly minified code portion
+    return true;
+  },
+  'as well as a sourcemap': function (topic) {
+    var actualFileMap = grunt.file.read(topic.actualMap);
+    assert.ok(actualFileMap, ' generates a source map for files');
+  }
+};
+
+// var commands =
+// function Suite() {
+
+// }
+// Suite
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -28,17 +63,17 @@ exports['jsmin-sourcemap'] = {
   'jsmin-sourcemap': function (test) {
     test.expect(6);
 
-    // suite.addBatch({
-    //   // jquery.js
-    //   'A single file': {
-    //     // grunt jsmin-sourcemap:single
-    //     'processed via JSMin': {
-    //       'outputs proper minified code': true,
-    //       'which points to the map file': true,
-    //       'as well as a sourcemap': true
-    //     }
-    //   }
-    // });
+    suite.addBatch({
+      // jquery.js
+      'A single file': {
+        // grunt jsmin-sourcemap:single
+        'processed via JSMin': {
+          'outputs proper minified code': true,
+          'which points to the map file': true,
+          'as well as a sourcemap': true
+        }
+      }
+    });
 
       var expectedSingle = grunt.file.read('expected/jquery.min.js'),
           actualSingle = grunt.file.read('actual/jquery.min.js'),
