1d6e1fd3534d82cd30127811e94270ae3feb6ef4
Todd Wolfson <todd.wolfson@ensighten.com>
2012-11-04 15:57:40 -0800

    Uncommented tests and all passing

--- test/grunt.js
+++ test/grunt.js
@@ -7,32 +7,32 @@ module.exports = function (grunt) {
       'version': '1.5.0'
     },
     'jsmin-sourcemap': {
-      // // Compact format -- https://github.com/gruntjs/grunt/blob/master/docs/api.md#thisfile--grunttaskcurrentfile
-      // 'actual/compact.min.js': 'test_files/jquery.js',
-      // // Normal format
-      // single: {
-        // src: 'test_files/jquery.js',
-        // dest: 'actual/jquery.min.js',
-        // destMap: 'actual/jquery.js.map'
-      // },
-      // multi: {
-        // src: ['test_files/jquery.js', 'test_files/underscore.js'],
-        // dest: 'actual/multi.min.js',
-        // destMap: 'actual/multi.js.map'
-      // },
-      // // Package specific interpolation
-      // interpolation: {
-        // src: 'test_files/jquery.js',
-        // dest: 'actual/interpolate.<%= pkg.name %>-<%= pkg.version %>.min.js',
-        // destMap: 'actual/interpolate.<%= pkg.name %>-<%= pkg.version %>.js.map'
-      // },
-      // // Nested files
-      // nested: {
-        // // Linux did not perform natural order sorting on first run
-        // // src: 'test_files/nested-src/*.js',
-        // src: ['test_files/nested-src/1.js', 'test_files/nested-src/3.js', 'test_files/nested-src/2.js'],
-        // dest: 'actual/nested-dest/nested.min.js'
-      // },
+      // Compact format -- https://github.com/gruntjs/grunt/blob/master/docs/api.md#thisfile--grunttaskcurrentfile
+      'actual/compact.min.js': 'test_files/jquery.js',
+      // Normal format
+      single: {
+        src: 'test_files/jquery.js',
+        dest: 'actual/jquery.min.js',
+        destMap: 'actual/jquery.js.map'
+      },
+      multi: {
+        src: ['test_files/jquery.js', 'test_files/underscore.js'],
+        dest: 'actual/multi.min.js',
+        destMap: 'actual/multi.js.map'
+      },
+      // Package specific interpolation
+      interpolation: {
+        src: 'test_files/jquery.js',
+        dest: 'actual/interpolate.<%= pkg.name %>-<%= pkg.version %>.min.js',
+        destMap: 'actual/interpolate.<%= pkg.name %>-<%= pkg.version %>.js.map'
+      },
+      // Nested files
+      nested: {
+        // Linux did not perform natural order sorting on first run
+        // src: 'test_files/nested-src/*.js',
+        src: ['test_files/nested-src/1.js', 'test_files/nested-src/3.js', 'test_files/nested-src/2.js'],
+        dest: 'actual/nested-dest/nested.min.js'
+      },
       // Different dests
       differentDest: {
         src: ['test_files/nested-src/1.js', 'test_files/nested-src/3.js', 'test_files/nested-src/2.js'],
--- test/jsmin-sourcemap.test.js
+++ test/jsmin-sourcemap.test.js
@@ -25,97 +25,97 @@ exports['jsmin-sourcemap'] = {
     // setup here
     done();
   },
-  // 'jsmin-sourcemap compact': function (test) {
-    // test.expect(4);
+  'jsmin-sourcemap compact': function (test) {
+    test.expect(4);
 
-      // var expectedCompact = grunt.file.read('expected/compact.min.js'),
-          // actualCompact = grunt.file.read('actual/compact.min.js'),
-          // actualCompactMap = grunt.file.read('actual/compact.min.js.map'),
-          // compactMapComment = '//@ sourceMappingURL=compact.min.js.map',
-          // compactMapDeclarativeExists = actualCompact.indexOf(compactMapComment) > -1;
-        // // outputs proper minified code
-        // test.strictEqual(actualCompact, expectedCompact, ' properly minifies a compact file');
-        // // which points to the map file
-        // test.ok(compactMapDeclarativeExists, ' points to the proper map location for the compact file');
-        // // and the map file points back to the minified code
-        // test.ok(actualCompactMap.indexOf('compact.min.js') > -1, ' points back to minified compact file');
-        // // as well as a sourcemap
-        // test.ok(actualCompactMap, ' generates a source map for a compact file');
+      var expectedCompact = grunt.file.read('expected/compact.min.js'),
+          actualCompact = grunt.file.read('actual/compact.min.js'),
+          actualCompactMap = grunt.file.read('actual/compact.min.js.map'),
+          compactMapComment = '//@ sourceMappingURL=compact.min.js.map',
+          compactMapDeclarativeExists = actualCompact.indexOf(compactMapComment) > -1;
+        // outputs proper minified code
+        test.strictEqual(actualCompact, expectedCompact, ' properly minifies a compact file');
+        // which points to the map file
+        test.ok(compactMapDeclarativeExists, ' points to the proper map location for the compact file');
+        // and the map file points back to the minified code
+        test.ok(actualCompactMap.indexOf('compact.min.js') > -1, ' points back to minified compact file');
+        // as well as a sourcemap
+        test.ok(actualCompactMap, ' generates a source map for a compact file');
 
-    // test.done();
-  // },
-  // 'jsmin-sourcemap single': function (test) {
-    // test.expect(3);
+    test.done();
+  },
+  'jsmin-sourcemap single': function (test) {
+    test.expect(3);
 
-      // var expectedSingle = grunt.file.read('expected/jquery.min.js'),
-          // actualSingle = grunt.file.read('actual/jquery.min.js'),
-          // actualSingleMap = grunt.file.read('actual/jquery.js.map'),
-          // singleMapComment = '//@ sourceMappingURL=jquery.js.map',
-          // singleMapDeclarativeExists = actualSingle.indexOf(singleMapComment) > -1;
-        // // outputs proper minified code
-        // test.strictEqual(actualSingle, expectedSingle, ' properly minifies a single file');
-        // // which points to the map file
-        // test.ok(singleMapDeclarativeExists, ' points to the proper map location for the single file');
-        // // as well as a sourcemap
-        // test.ok(actualSingleMap, ' generates a source map for a single file');
+      var expectedSingle = grunt.file.read('expected/jquery.min.js'),
+          actualSingle = grunt.file.read('actual/jquery.min.js'),
+          actualSingleMap = grunt.file.read('actual/jquery.js.map'),
+          singleMapComment = '//@ sourceMappingURL=jquery.js.map',
+          singleMapDeclarativeExists = actualSingle.indexOf(singleMapComment) > -1;
+        // outputs proper minified code
+        test.strictEqual(actualSingle, expectedSingle, ' properly minifies a single file');
+        // which points to the map file
+        test.ok(singleMapDeclarativeExists, ' points to the proper map location for the single file');
+        // as well as a sourcemap
+        test.ok(actualSingleMap, ' generates a source map for a single file');
 
-    // test.done();
-  // },
-  // 'jsmin-sourcemap multi': function (test) {
-    // test.expect(3);
-    // // Multiple files (jquery.js + underscore.js)
-      // // processed via JSMin (grunt jsmin-sourcemap:multi)
-      // var expectedMulti = grunt.file.read('expected/multi.min.js'),
-          // actualMulti = grunt.file.read('actual/multi.min.js'),
-          // actualMultiMap = grunt.file.read('actual/multi.js.map'),
-          // multiMapComment = '//@ sourceMappingURL=multi.js.map',
-          // multiMapDeclarativeExists = actualMulti.indexOf(multiMapComment) > -1;
-        // // outputs proper minified code
-        // test.strictEqual(actualMulti, expectedMulti, ' properly minifies multiple files');
-        // // which points to the map file
-        // test.ok(multiMapDeclarativeExists, ' points to the proper map location for the multiple files');
-        // // as well as a sourcemap
-        // test.ok(actualMultiMap, ' generates a source map for multiple files');
+    test.done();
+  },
+  'jsmin-sourcemap multi': function (test) {
+    test.expect(3);
+    // Multiple files (jquery.js + underscore.js)
+      // processed via JSMin (grunt jsmin-sourcemap:multi)
+      var expectedMulti = grunt.file.read('expected/multi.min.js'),
+          actualMulti = grunt.file.read('actual/multi.min.js'),
+          actualMultiMap = grunt.file.read('actual/multi.js.map'),
+          multiMapComment = '//@ sourceMappingURL=multi.js.map',
+          multiMapDeclarativeExists = actualMulti.indexOf(multiMapComment) > -1;
+        // outputs proper minified code
+        test.strictEqual(actualMulti, expectedMulti, ' properly minifies multiple files');
+        // which points to the map file
+        test.ok(multiMapDeclarativeExists, ' points to the proper map location for the multiple files');
+        // as well as a sourcemap
+        test.ok(actualMultiMap, ' generates a source map for multiple files');
 
-    // test.done();
-  // },
-  // 'jsmin-sourcemap interpolate': function (test) {
-    // test.expect(3);
-    // // Interpolated file paths
-    // var interpolationStr = 'grunt-jsmin-sourcemap-1.5.0',
-        // expectedInterpolate = grunt.file.read('expected/interpolate.' + interpolationStr + '.min.js'),
-        // actualInterpolate = grunt.file.read('actual/interpolate.' + interpolationStr + '.min.js'),
-        // actualInterpolateMap = grunt.file.read('actual/interpolate.' + interpolationStr + '.js.map'),
-        // interpolateMapComment = '//@ sourceMappingURL=interpolate.' + interpolationStr + '.js.map',
-        // interpolateMapDeclarativeExists = actualInterpolate.indexOf(interpolateMapComment) > -1;
+    test.done();
+  },
+  'jsmin-sourcemap interpolate': function (test) {
+    test.expect(3);
+    // Interpolated file paths
+    var interpolationStr = 'grunt-jsmin-sourcemap-1.5.0',
+        expectedInterpolate = grunt.file.read('expected/interpolate.' + interpolationStr + '.min.js'),
+        actualInterpolate = grunt.file.read('actual/interpolate.' + interpolationStr + '.min.js'),
+        actualInterpolateMap = grunt.file.read('actual/interpolate.' + interpolationStr + '.js.map'),
+        interpolateMapComment = '//@ sourceMappingURL=interpolate.' + interpolationStr + '.js.map',
+        interpolateMapDeclarativeExists = actualInterpolate.indexOf(interpolateMapComment) > -1;
 
-      // // outputs proper minified code
-      // test.strictEqual(actualInterpolate, expectedInterpolate, ' properly minifies a interpolated file');
-      // // which points to the map file
-      // test.ok(interpolateMapDeclarativeExists, ' points to the proper map location for the interpolated file');
-      // // as well as a sourcemap
-      // test.ok(actualInterpolateMap, ' generates a source map for at the interpolated location');
+      // outputs proper minified code
+      test.strictEqual(actualInterpolate, expectedInterpolate, ' properly minifies a interpolated file');
+      // which points to the map file
+      test.ok(interpolateMapDeclarativeExists, ' points to the proper map location for the interpolated file');
+      // as well as a sourcemap
+      test.ok(actualInterpolateMap, ' generates a source map for at the interpolated location');
 
-    // test.done();
-  // },
-  // 'jsmin-sourcemap nested': function (test) {
-    // test.expect(3);
+    test.done();
+  },
+  'jsmin-sourcemap nested': function (test) {
+    test.expect(3);
 
-    // // Multiple nested files
-      // // processed via JSMin
-      // var expectedNested = grunt.file.read('expected/nested-dest/nested.min.js'),
-          // actualNested = grunt.file.read('actual/nested-dest/nested.min.js'),
-          // actualNestedMap = grunt.file.read('actual/nested-dest/nested.min.js.map'),
-          // nestedMapComment = '//@ sourceMappingURL=nested.min.js.map',
-          // nestedMapDeclarativeExists = actualNested.indexOf(nestedMapComment) > -1;
-        // // outputs proper minified code
-        // test.strictEqual(actualNested, expectedNested, ' properly minifies nested files');
-        // // which points to the map file
-        // test.ok(nestedMapDeclarativeExists, ' points to the proper map location for the nested files');
-        // // as well as a sourcemap
-        // test.ok(actualNestedMap, ' generates a source map for nested files');
-    // test.done();
-  // },
+    // Multiple nested files
+      // processed via JSMin
+      var expectedNested = grunt.file.read('expected/nested-dest/nested.min.js'),
+          actualNested = grunt.file.read('actual/nested-dest/nested.min.js'),
+          actualNestedMap = grunt.file.read('actual/nested-dest/nested.min.js.map'),
+          nestedMapComment = '//@ sourceMappingURL=nested.min.js.map',
+          nestedMapDeclarativeExists = actualNested.indexOf(nestedMapComment) > -1;
+        // outputs proper minified code
+        test.strictEqual(actualNested, expectedNested, ' properly minifies nested files');
+        // which points to the map file
+        test.ok(nestedMapDeclarativeExists, ' points to the proper map location for the nested files');
+        // as well as a sourcemap
+        test.ok(actualNestedMap, ' generates a source map for nested files');
+    test.done();
+  },
   'jsmin-sourcemap differentDest': function (test) {
     test.expect(4);
 
